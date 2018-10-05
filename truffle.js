467685063a900ca4d8616b6868563b61cb7e571c
David Burela <David.Burela@gmail.com>
2018-10-05 16:23:36 -0700

    deploy to azure poa

--- truffle.js
+++ truffle.js
@@ -1,3 +1,6 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = "plug gaze game pact lamp mean pilot bread parent finger logic mercy";
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -5,6 +8,15 @@ module.exports = {
     reporter: "mocha-junit-reporter",
     reporterOptions: {
       mochaFile: 'TEST-truffle.xml'
+    }
+  },
+  networks: {
+    azure: {
+      provider: function () {
+        return new HDWalletProvider(mnemonic, "http://eth5kzzgs-dns-reg1.westus.cloudapp.azure.com:8540", 0)
+      },
+      gasPrice : 0,
+      network_id: "*"
     }
   }
 };
